62cc8fb73cec4d0882ffd8973fb20fae2a8a40b7
sonu_secops <laxmikantbehera042@gmail.com>
2026-07-24 13:23:42 +0530

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -14,7 +14,7 @@ function askAI() {
     response.innerHTML = "Thinking...";
 
     // Send request to Flask backend
-    fetch("http://127.0.0.1:5000/ask", {
+    fetch("/ask", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
